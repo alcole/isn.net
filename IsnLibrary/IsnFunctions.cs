15223e5e0ca567370986a53688e7edba07e1b1a0
alex-cole <alexjcole01@gmail.com>
2019-10-16 22:58:26 +0100

    Add project files.

---
+++ IsnLibrary/IsnFunctions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace IsnLibrary
+{
+    public static class IsnFunctions
+    {
+        public static bool isValid(String isn, String type)
+        {
+            return true;
+        }
+
+        public static String getCanonical(String isn, String type)
+        {
+            //strip dashes
+            //lower x to upper X
+            //ISBN 10 to ISBN 13
+            //EAN-ISSN to ISSN
+            return "111";
+        }
+
+        public static String getType(String isn)
+        {
+            return "ISBN";
+        }
+
+        //couldhave types as an enum class?
+    }
+}
