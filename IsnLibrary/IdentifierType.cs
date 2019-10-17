6e9143b1e0dac459687f57f0b52a7c820d32db79
alex-cole <alexjcole01@gmail.com>
2019-10-17 22:17:46 +0100

    added identifier class and supporting classes

---
+++ IsnLibrary/IdentifierType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IsnLibrary
+{
+    class IdentifierType
+    {
+        public enum IdType { ISBN, ISBN10, ISMN, ISSN, EANISSN, EAN, ISNI, ISPI, Other}
+    }
+}
