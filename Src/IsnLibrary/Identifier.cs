5c51b7f9b0c629ea0280449dd65745ac27ff5097
Kieran Burke <kieran314237@hotmail.co.uk>
2019-10-22 15:36:39 +0900

    [add] initial regex implementation

--- Src/IsnLibrary/Identifier.cs
+++ Src/IsnLibrary/Identifier.cs
@@ -1,15 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace IsnLibrary
 {
+    public static class Constants {
+        public static string SEP = "(?:\\-|\\s)";
+        public static string GROUP = "(\\d{1,5})";
+        public static string PUBLISHER = "(\\d{1,7})";
+        public static string TITLE = "(\\d{1,6})";
+    }
     public class ISBN : Identifier
     {
         public ISBN(string number) : base(number) {}
 
         public string ISBN10 { get { return Number.Length == 13 ? Number.Substring(3) : Number; } }
         public string ISBN13 { get { return Number.Length == 13 ? Number : $"978{Number}"; } }
+
+        protected override string RegexPattern { get { return "^(978|979)(?:(\\d{10})|(?:" + Constants.SEP + Constants.GROUP + Constants.SEP + Constants.PUBLISHER + Constants.SEP + Constants.TITLE + Constants.SEP + "([0-9])))$"; } }
 
         public override bool Validate()
         {
@@ -47,6 +56,8 @@ namespace IsnLibrary
     {
         protected string Number { get; }
 
+        protected virtual string RegexPattern { get { return ".*"; } }
+
         public Identifier(string number)
         {
             Number = number;
@@ -54,8 +65,8 @@ namespace IsnLibrary
 
         public virtual bool Validate()
         {
-
-            return true;
+            bool regexPass = Regex.IsMatch(Number, RegexPattern);
+            return regexPass;
         }
 
         public override string ToString()
